619b382a701d9a6f263cf92ae414434d428d7c97
Margo Bobyleva <shinebebi@yandex.ru>
2021-01-25 20:13:16 +0300

    refactor: различные измененя в интерфейсе сайта

--- script/script.js
+++ script/script.js
@@ -1,26 +1,28 @@
-let likeBtn = document.querySelector('.element__like-btn');
 let editBtn = document.querySelector('.profile__edit-btn');
 let closeBtn = document.querySelector('.popup__close-btn');
 let submitBtn = document.querySelector('.popup__submit-btn');
 let formElement = document.querySelector('.popup__container');
 let nameInput = formElement.querySelector('.popup__user-name');
 let jobInput = formElement.querySelector('.popup__user-job');
-function getLike () {
-    likeBtn.classList.toggle('element__like-btn_active');
-}
+let editName = document.querySelector('.profile__user-name');
+let editJob = document.querySelector('.profile__user-profession');
 function editProfile () {
     let popup = document.querySelector('.popup')
     popup.classList.toggle('popup_opened');
 }
+function closeProfile () {
+    let popup = document.querySelector('.popup')
+    popup.classList.toggle('popup_opened');
+    nameInput.value = editName.textContent;
+    jobInput.value = editJob.textContent;
+}
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    let editName = document.querySelector('.profile__user-name');
-    let editJob = document.querySelector('.profile__user-profession');
     editName.textContent = nameInput.value;
     editJob.textContent = jobInput.value;
+    editProfile()
 }
-submitBtn.addEventListener('click', editProfile);
-likeBtn.addEventListener('click', getLike);
-editBtn.addEventListener('click', editProfile);
 submitBtn.addEventListener('click', handleFormSubmit);
-closeBtn.addEventListener('click', editProfile);
+editBtn.addEventListener('click', editProfile);
+formElement.addEventListener('submit', handleFormSubmit);
+closeBtn.addEventListener('click', closeProfile);
